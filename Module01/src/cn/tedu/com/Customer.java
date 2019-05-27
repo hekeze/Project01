127154f49bc608c74f3dc6c1b093ddcc8fd68393
hekeze <18682153857@163.com>
2019-05-27 15:48:55 +0800

    aaaaa

---
+++ Module01/src/cn/tedu/com/Customer.java
@@ -0,0 +1,9 @@
+package cn.tedu.com;
+
+
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("args = [" + args + "]");
+    }
+
+}
---
+++ Module01/src/cn/tedu/com/Demo01.java
@@ -0,0 +1,9 @@
+package cn.tedu.com;
+
+/**
+ * @outhor hekeze
+ * @date 2019-05-27
+ */
+public class Demo01 {
+
+}
